efcdc6f7245f1f9e1e008ef40c0c367d0d78bc35
sathvick <kesathvick@gmail.com>
2025-03-27 14:49:51 +0530

    Adding all the necessary files

---
+++ views.py
@@ -0,0 +1,88 @@
+import re
+import sys
+import csv
+
+def add(i):
+    with open('data.csv', 'a+', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(i)
+
+#add(['Unknown','F','12345','who@gmail.com'])
+
+def view():
+    data = []
+    with open('data.csv') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            data.append(row)
+    print(data)
+    return data
+
+def view1():
+    flag=0
+    data = []
+    with open('data.csv') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            data.append(row[2])
+    print(data)
+    return data
+
+
+def remove(i):
+    def save(j):
+        with open('data.csv', 'w', newline='') as file:
+            writer=csv.writer(file)
+            writer.writerows(j)
+    
+    new_list = []
+    telephone = i
+
+    with open('data.csv') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            new_list.append(row)
+
+            for element in row:
+                if element == telephone:
+                    new_list.remove(row)
+    save(new_list)
+
+#view()
+
+def update(i):
+
+    def update_newlist(j):
+        with open('data.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerows(j)
+    new_list = []
+    telephone = i[0]
+
+    with open('data.csv','r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            new_list.append(row)
+            for element in row:
+                if element == telephone:
+                    name = i[1]
+                    gender = i[2]
+                    telephone = i[3]
+                    email = i[4]
+
+                    data = [name,gender,telephone,email]
+                    index = new_list.index(row)
+                    new_list[index] = data
+    update_newlist(new_list)
+
+def search(i):
+    data = []
+    telephone = i
+
+    with open('data.csv', 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            for element in row:
+                if element == telephone:
+                    data.append(row)
+    return data
